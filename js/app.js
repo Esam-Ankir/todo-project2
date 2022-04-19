5aad455c4f51c4743c76d8bd4457be0ef3c8d6cd
Esam-Ankir <eankir@hotmail.com>
2022-04-19 05:49:18 +0300

    new todo project

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function btn1() {
+    document.getElementById("btn1").style.background = "green";
+  }
+  function btn2() {
+    document.getElementById("btn2").style.background = "green";
+  }
+  function btn3() {
+    document.getElementById("btn3").style.background = "green";
+  }
+  function btn4() {
+    document.getElementById("btn4").style.background = "green";
+  }
